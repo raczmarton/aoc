09f9c655d876bc5d0f2c3e3940e907117f4bbe17
Marton Racz <raczmarton292@gmail.com>
2024-12-01 18:43:29 +0100

    Day 1 in OCaml part2

--- d1/ocaml/bin/main.ml
+++ d1/ocaml/bin/main.ml
@@ -10,7 +10,7 @@ let split_list lst =
   in
   aux lst [] []
 
-
+module IntMap = Map.Make(Int)
 let read_file file : string list = 
     let ic = open_in file in
     let try_read () = 
@@ -20,16 +20,40 @@ let read_file file : string list =
         | None -> close_in ic; List.rev acc in
     reader []
 
-let () = 
-    let lines = read_file file in
+
+let sort_split_lines lines : int list * int list =
     let (list1, list2) = split_list lines in
     let int_l1 = List.map int_of_string list1  in
     let int_l2 = List.map int_of_string list2 in
     let sorted_l1 = List.sort compare int_l1 in
     let sorted_le = List.sort compare int_l2 in
-    let distances = List.map2( - ) sorted_l1 sorted_le |> List.map Int.abs in
+    (sorted_l1, sorted_le)
+
+let find_distance sorted_l1 sorted_l2 : int = 
+    let distances = List.map2( - ) sorted_l1 sorted_l2 |> List.map Int.abs in
     let dist = List.fold_left ( + ) 0 distances in
-    let out = string_of_int dist in
-    let dbg = String.concat " ; " (List.map string_of_int sorted_l1) in
-    let _u = print_endline dbg in
-    print_endline  out
+    (dist)
+
+let similarity sorted_l1 sorted_l2 : int = 
+   let scores = List.fold_left (fun acc v -> (IntMap.update v (fun mape -> 
+    match mape with 
+    | Some opt -> Some(opt+1)
+    | None -> Some(1)) acc )) IntMap.empty sorted_l2 in
+
+   let result =  (List.fold_left (fun acc v -> 
+        match IntMap.find_opt v scores with 
+    | Some opt -> acc + (opt * v)
+    | None -> acc
+    ) 0 sorted_l1) in
+   (result)
+
+
+let () =
+    let lines = read_file file in
+    let (sorted_l1, sorted_l2) = sort_split_lines lines in
+    let sim = similarity sorted_l1 sorted_l2 in
+    let abs_dist = find_distance sorted_l1 sorted_l2 in
+    let fmt = format_of_string "Similarity: %d \nAbsolute distance: %d \n" in
+    let formatted_string = Printf.sprintf fmt sim abs_dist in
+    print_endline formatted_string
+    
